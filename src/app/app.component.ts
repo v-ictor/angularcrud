1fe173debacd1603046e01be96c70afe7ecb7281
victor erikson <zetsuengt@gmail.com>
2020-02-17 19:14:47 -0600

    crud employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularcrud';
+  // title = 'angularcrud';
+  employeeArray: Employee[] = [
+    {id: 1, name: "Ryan", country: "USA"},
+    {id: 2, name: "Angelica", country: "USA"},
+    {id: 3, name: "Joe", country: "USA"}
+  ];
+  
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id == 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  delete(){
+    if(confirm('Are you sure yu want to be delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
